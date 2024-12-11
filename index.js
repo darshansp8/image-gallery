a6889fcd89a380392e4aa183faac38f0ab3212d4
Darshan Sakpal <d.sakpal38@gmail.com>
2024-12-11 02:49:41 +0000

    Implemented logic for keyboard navigation through images

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
 'use strict';
 
+document.addEventListener("DOMContentLoaded", () => {
+    const galleryItems = document.querySelectorAll('.gallery-item');
+    const cards = document.querySelectorAll(".card");
+    let currentIndex = 0;
+    console.log(cards.length)
+
+    const focusItem = (index) => {
+        if (index >= 0 && index < cards.length) {
+            cards[index].focus();
+            currentIndex = index;
+        }
+    };
+
+    document.addEventListener('keydown', (e) => {
+        switch (e.key) {
+            case 'ArrowRight':
+                focusItem(currentIndex + 1);
+                console.log(currentIndex)
+                break;
+            case 'ArrowLeft':
+                focusItem(currentIndex - 1);
+                break;
+            case 'ArrowDown':
+                if (currentIndex + 3 < galleryItems.length) {
+                    focusItem(currentIndex + 3);
+                }
+                break;
+            case 'ArrowUp':
+                if (currentIndex - 3 > 0) {
+                    focusItem(currentIndex - 3);
+                }
+                break;
+        }
+    });
+    cards.forEach(item => {
+        item.addEventListener('focus', () => {
+            console.log("Focused", item)
+            item.classList.add('focused');  // Add a class to style the focused item
+        });
+        item.addEventListener('blur', () => {
+            item.classList.remove('focused');  // Remove the class when the focus is removed
+        });
+    });
+})
+
 // Logic to filter images starts
 
 const filterButtons = document.querySelectorAll('.gallery-filter-item');
